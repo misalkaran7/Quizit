576a38c331ad3499ee848a5841fe74cfb1e9c2cd
misalkaran7 <misalkaran6@gmail.com>
2026-07-20 12:12:52 +0530

    Update API configuration for Vercel services deployment

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running smoothly on port ${PORT}`);
 });
